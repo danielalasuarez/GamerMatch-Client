0b98853a2717d553a9f11de3082f8034e217cf1c
danielalasuarez <69167761+danielalasuarez@users.noreply.github.com>
2020-12-14 13:07:07 -0500

    got card to display correctly and swipe functions working

--- gamermatch-client/src/components/GamerCards.js
+++ gamermatch-client/src/components/GamerCards.js
@@ -1,3 +1,4 @@
+import { SwipeableDrawer } from '@material-ui/core';
 import React, { useState } from 'react'
 //  note: found this really cool package called react tinder cards that I will be using for this component to give the card the swipe functionality
 // source https://www.npmjs.com/package/react-tinder-card 
@@ -7,7 +8,7 @@ import "../style/gamerCards.css"
 
 
 
-// on line 11 we are storing the people in the state and initializing it with what the cards will have inside (switch to schema file later)
+// on line 13 we are storing the people in the state and initializing it with what the cards will have inside (switch to schema file later)
 function GamerCards() {
     const [people, setPeople] = useState([
         {
@@ -19,14 +20,44 @@ function GamerCards() {
             url: 'https://i.imgur.com/JNPw9YC.jpg',
             },
     ]);
+
+    // below are function that we are going to pass below on the display of the card
+
+    //function to swipe card .... console logs when the person swipes left
+    const swiped = (direction, nameDelete) => {
+        console.log("removing gamer: " + nameDelete);
+        // setNewDirection(direction);
+    };
+
+    //function to make the card not visible.... console logs when the card is gone
+    const outOfFrame = (name) => {
+        console.log("gamer " + name + " no longer on the screen");
+    };
+
+
     return (
         <div className="gamerCards">
             <div className="gamerCards__cardContainer">
             {/* for every single gamer then the following will display */}
             {people.map((person) => (
                 // <h1>{person.name}</h1> // works!!!!!!
-                <TinderCard>
+                <TinderCard
+                className="swipe"
+                key={person.name}
+                preventSwipe={['up', 'down']} //does not allow to swipe up and down 
+                //function below puts the card out of frame if it is swiped to the left and will console log because of the swiped function 
+                onSwipe={(dir) => swiped(dir, person.name)}
+                onCardLeftScreen={() => outOfFrame(person.name)} //this function runs when the card is out of the frame
+                >
+                    {/* inside the <TinderCard> goes the below div which styles the layout of card */}
+                    {/* this div has a background of the gamer image (used inline styling) and INSIDE goes the name and stats.. this will overlay the info on the image */}
+                    <div
+                    style={{ backgroundImage: 'url(' + person.url + ') '}}
+                    className='card'
+                    >
 
+                        <h3>{person.name}</h3>
+                    </div>
                 </TinderCard>
             ))}
 
