90aeaa5b507db06a652683703ced95b7b2b711c7
danielalasuarez <69167761+danielalasuarez@users.noreply.github.com>
2020-12-15 12:57:35 -0500

    WORKING I HOOKED UP THE BACKEND TO THE FRONT need to figure out why there is an empty card

--- gamermatch-client/src/components/GamerCards.js
+++ gamermatch-client/src/components/GamerCards.js
@@ -1,26 +1,48 @@
 import { SwipeableDrawer } from '@material-ui/core';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 //  note: found this really cool package called react tinder cards that I will be using for this component to give the card the swipe functionality
 // source https://www.npmjs.com/package/react-tinder-card 
 import TinderCard from "react-tinder-card";
 import "../style/gamerCards.css"
+import axios from '../axios'
 
 
 
 
-// on line 13 we are storing the people in the state and initializing it with what the cards will have inside (switch to pull from back end and schema file later)
+// on line 14 we are storing the people in the state and initializing it with what the cards will have inside (switch to pull from back end and schema file later)
 function GamerCards() {
-    const [people, setPeople] = useState([
-        {
-        name: 'Gamer Tag goes here',
-        url: 'https://i.imgur.com/JNPw9YC.jpg',
-        },
-        {
-            name: 'coolgamer2020',
-            url: 'https://i.imgur.com/JNPw9YC.jpg',
-            },
-    ]);
+    // const [people, setPeople] = useState([
+    //     {
+    //     name: 'Gamer Tag goes here',
+    //     url: 'https://i.imgur.com/JNPw9YC.jpg',
+    //     },
+    //     {
+    //         name: 'coolgamer2020',
+    //         url: 'https://i.imgur.com/JNPw9YC.jpg',
+    //         },
+    // ]);
 
+    //the below is the new version of the hard coded cards above because we will be using the scheam created in the backend
+    //empty array
+    const [people, setPeople] = useState([]);
+
+    //useEffect will occur when the tinder cards load
+    //it will run the below piece of code and not run it again
+    //same as component did mount
+    //in the [] at the end you can add [name] id the name changes and it will refire the code and run again once 
+    useEffect(() => {
+//async function below
+        async function fetchData() {
+            const request = await axios.get("/gamers/cards") // calling our endpoint from back
+            
+            //setting the people variable on line 27 whatever request.data comes back as
+            setPeople(request.data);
+        }
+//calling it below
+        fetchData();
+    }, []) 
+
+    console.log(people)
     // below are function that we are going to pass below on the display of the card
 
     //function to swipe card .... console logs when the person swipes left
@@ -53,7 +75,8 @@ function GamerCards() {
                     {/* inside the <TinderCard> goes the below div which styles the layout of card */}
                     {/* this div has a background of the gamer image (used inline styling) and INSIDE goes the name and stats.. this will overlay the info on the image */}
                     <div
-                    style={{ backgroundImage: 'url(' + person.url + ') '}}
+                    //took me forever to debug. i had person.url originally and in the schema i wrote imgURL 
+                    style={{ backgroundImage: 'url(' + person.imgUrl + ') '}}
                     className='card'
                     >
 
