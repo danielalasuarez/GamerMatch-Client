d74fc5baf07005d992550f41acaf8920fde265c0
danielalasuarez <69167761+danielalasuarez@users.noreply.github.com>
2020-12-15 23:03:36 -0500

    added chat functionality with a working form need to create profile form and hook to backend and then hook chat to mongo

--- gamermatch-client/src/components/ActiveChat.js
+++ gamermatch-client/src/components/ActiveChat.js
@@ -8,7 +8,10 @@ import { Link } from "react-router-dom";
 //this is hard coded for now if time i want to add it to the backend and save the chat data there and use websocket
 
 function ActiveChat() {
+//to get the input to work and set the input state
+const [input, setInput] = useState('');
 
+//to set the messages 
     const [messages, setMessages] = useState ([
         {
         name: 'dani',
@@ -24,6 +27,16 @@ function ActiveChat() {
                 message: "i'm down to play, add me"
                 },
     ])
+//function that will take what the user types in the input and sends it where we want 
+    const handleSend = e => {
+        e.preventDefault(); //if the event.preventDefault is not added it will refresh when you press enter on keyboard
+        //deally this will be sent to the data base if hooked to backend
+        // the below will set setMessages to an array and grab whatever was in that array already which is ...messages
+        //as well as a new message with whatever you've typed in your input which is {message:input}
+        setMessages([...messages, {message: input}]);
+        //reset the input
+        setInput('');
+    }
     return (
         <div className="active__chat">
             <p className="active__chat__match__message"> YOU MATCHED WITH DANI! SET UP THE DETAILS AND START GAMING</p>
@@ -57,9 +70,13 @@ function ActiveChat() {
                 </IconButton>
             </Link>
                     <input 
+                    onChange={e => setInput(e.target.value)} //every time we type in the input feild(on chnage) its going to update 
+                    value={input} // here we set the input value to equal the input state we set above 
                     className="active__chat__input__box"
                     type="text" />
-                    <button className="active__chat__input__button">SEND</button>
+
+{/* handleSend is the function we created above which will send what is in the input feild to where we want it   */}
+                    <button onClick={handleSend} type="submit" className="active__chat__input__button">SEND</button> 
                     
                 </form>
                
