f5ab4e62a296d7b5b8cb373b6cba5da1bef68cbc
danielalasuarez <69167761+danielalasuarez@users.noreply.github.com>
2020-12-17 15:19:54 -0500

    register form working and sent to back success

--- gamermatch-client/src/components/auth/Register.js
+++ gamermatch-client/src/components/auth/Register.js
@@ -1,12 +1,107 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import UserContext from '../../context/UserContext';
 import ArrowBackTwoToneIcon from '@material-ui/icons/ArrowBackTwoTone';
 import IconButton from '@material-ui/core/IconButton';
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
+import "../../style/register.css"
+import axios from '../../axios'
+
+//this form will register the user in the DB 
+// then automatically log the user in upon registering 
+//then redirect to the homepage 
 
 function Register() {
+//store the values from each input field in the state 
+
+    const[name, setName] = useState();
+    const[email, setEmail] = useState();
+    const[password, setPassword] = useState();
+    const[imgUrl, setImgUrl] = useState();
+    const[killDeathRatio, setKillDeathRatio] = useState();
+    const[gameHighlights, setGameHighlights] = useState();
+
+    //enable the context we imported 
+    const {setUserData} = useContext(UserContext);
+
+    //enable the histroy
+    const history = useHistory();
+
+    //function that will submit our form
+    const submit = async (event) => {
+        event.preventDefault(); //to prevent reloading and emptying out before it is sent to back
+        const newUser = {name, email, password, imgUrl, killDeathRatio, gameHighlights}; //grabbing the state which is what the user inputted
+        await axios.post("/profile/register", newUser); //newUser is the updated states and the data we will post tp /profile/register
+    //user is created now below we will log in the user 
+    const loginResponse = await axios.post("/profile/login",{ //the only data we need to log in is email and password so we will make an object what only grabs those two so we only send those two to the back 
+        email, 
+        password,
+    }); //this gives us a response w a token and userData 
+    //this will set the context
+    setUserData({
+        token: loginResponse.data.token, // the token in the user data is the loginresponse.data.token(this .token is refering to the token in the backend )
+        user: loginResponse.data.user,
+    });
+    //this will set the token to the local storage for later use 
+    localStorage.setItem("auth-token", loginResponse.data.token);
+    history.push("/"); //redirect to home 
+    }; //logged in!!!! 
+
     return (
         <div>
             <h1>SIGN UP</h1>
+        <form onSubmit={submit}>
+        {/* name  */}
+            <label htmlFor="register__gamerTag">Gamer Tag</label> 
+            <input 
+            id="register__gamerTag" 
+            type="text" 
+            onChange={(e) => setName(e.target.value)} //on change changes the state of name 
+            />
+        {/* email */}
+            <label htmlFor="register__email">Email</label>
+            <input 
+            id="register__emai" 
+            type="email" 
+            onChange={(e) => setEmail(e.target.value)}
+            />
+        {/* password */}
+            <label htmlFor="register__password">Password</label>
+            <input 
+            id="register__password" 
+            type="password" 
+            onChange={(e) => setPassword(e.target.value)}
+            />
+        {/* imgUrl */}
+            <label htmlFor="register__imgUrl">Profile Picture</label>
+            <input 
+            id="register__imgUrl" 
+            type="text" 
+            onChange={(e) => setImgUrl(e.target.value)}
+            />
+        {/* killDeathRatio  */}
+            <label htmlFor="register__killDeathRatio">Kill Death Ratio</label>
+            <input 
+            id="register__killDeathRatio" 
+            type="text" 
+            onChange={(e) => setKillDeathRatio(e.target.value)}
+            />
+        {/* gameHighlights */}
+            <label htmlFor="register__gameHighlights">Game Highlights</label>
+            <input 
+            id="gameHighlights" 
+            type="text" 
+            onChange={(e) => setGameHighlights(e.target.value)}
+            />
+        {/* submit  */}
+            <input type="submit" value="Register" />
+
+        </form>
+
+
+
+
+
+
 
             <Link to='/profile'>
                 <IconButton className='back'>
